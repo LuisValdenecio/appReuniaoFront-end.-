92b04f935a8d7728adfbf195e1aa779098b26e04
Luis Valdenêncio <ls04af@gmail.com>
2019-05-12 13:55:31 -0300

    descricao de dados individuais da turma DONE

--- src/app/views/adminDashboard/turmas/turma/turma.component.ts
+++ src/app/views/adminDashboard/turmas/turma/turma.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from "@angular/router";
+import { DataModelInterface } from 'src/app/dataModels/DataModelInterface';
 
 @Component({
   selector: 'app-turma',
@@ -10,31 +11,43 @@ export class TurmaComponent  {
 
   private routeInfo : any;
   public display : Boolean = true; // --> isto é somente um hack para fazer desaparecer o view da turma
+  public _studentData : any[];
+  public _thisClassSubjects : any[];
+  private thisClassesURL = window.location.href.split("/")[5]; // --> Substituição urgente (dependencia com o backend)
+  private filteredAttribute : String = ""; // --> Substituição urgente (dependencia com o backend)
 
-  constructor(private activeRoute : ActivatedRoute) {
+  constructor(private activeRoute : ActivatedRoute, private dataModelInterface : DataModelInterface) {
     this.activeRoute.url.subscribe(data=>{
       this.routeInfo = data;
     });
-   }
 
-  public barChartOptions = {
-    scaleShowVerticalLines: false,
-    responsive: true
-  };
+    this.dataModelInterface.getAllStudents().subscribe(data=>{
+      this._studentData = data.filter(turma=> turma['turma_id'] == this.formatURL())
+    });
+
+    this.dataModelInterface.getAllSubjects("/"+this.formatURL()).subscribe(data=>{
+      this._thisClassSubjects = data;
+    });
+  }
 
   // --> isto é somente um hack para fazer desaparecer o view da turma
   public deleteParentView() {
     this.display = false;
   }
   
-  public barChartLabels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-  public barChartType = 'bar';
-  public barChartLegend = true;
-  
-  public barChartData = [
-    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
-    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
-  ];
+   // --> Substituição urgente (dependencia com o backend)
+   public formatURL() {
+
+    this.filteredAttribute = "";
+
+    for (let counter = 0; counter < this.thisClassesURL.length; counter++) {
+      this.filteredAttribute += this.thisClassesURL.charAt(counter);
+      if (counter == 7 || counter == 11 || counter == 15 || counter == 19) {
+        this.filteredAttribute += "-"
+      }
+    }
+    return this.filteredAttribute;
+  }
 
 
 
